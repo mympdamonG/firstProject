ca1a4a4852ebfe5194a8a1762520bf6075eceff1
mympdamonG <mympdamon@gmail.com>
2022-11-15 10:13:23 +0900

    1st

---
+++ firstApp/templatetags/custom_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from django.utils.html import mark_safe
+from datetime import time, datetime, date, timedelta
+
+register = template.Library()
+
+@register.filter(name="email_ma")
+def email_masker(value):
+    email_split = value.split("@")
+    return f"{email_split[0]}@******.***"
+
+@register.filter(name="name_ma")
+def name_masker(value):
+
+    name_split = value[:4]
+    return f"{name_split}####"
+
+@register.simple_tag(name="test_tags",takes_context=True) 
+def test_tags(context):
+    # for c in context:
+    #     print(c) 
+    tag_html = "<span class='badge badge-primary'>테스트 태그</span>"      
+
+    return mark_safe(tag_html)
